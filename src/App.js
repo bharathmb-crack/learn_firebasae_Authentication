0c9f0a8d0e9c12ed4882e0a5634713ac1ed01f5a
bharathmb-crack <bharathmbyahtti@gmail.com>
2022-10-20 23:04:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3> Register </h3>
+        <input type="text" placeholder="Email..." />
+        <input type="text" placeholder="Password..." />
+        <button>Create User</button>
+      </div>
+
+      <div>
+        <h3>Login</h3>
+        <input type="text" placeholder="Email..." />
+        <input type="text" placeholder="Password..." />
+        <button>Login</button>
+      </div>
+      <div>
+        <div>
+          <span>
+            <p>Logged user name:</p> loged name here
+          </span>
+          <button>sign out</button>
+        </div>
+      </div>
     </div>
   );
 }
